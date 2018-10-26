1a89dcd9d2567a08e6a7f9974889a4d0c3f96af1
alinemati <alinemati@live.com>
2018-10-26 23:40:31 +0330

    #8 get header info for any changes in diff

--- mineRepoCommits.py
+++ mineRepoCommits.py
@@ -1,6 +1,26 @@
 from pydriller import RepositoryMining
 from pydriller.domain.commit import ModificationType
 
+def getFirstOfChunkInfo(line):
+    infoParts = line.strip().split('@@')
+    if len(infoParts) == 3: # check if the format is correct
+        className = None
+        if ' class ' in infoParts[2]: # get class name if available
+            headerWords = infoParts[2].split()
+            className = headerWords[headerWords.index('class') + 1]
+        linesInfo = infoParts[1].split() 
+        linesInfo = linesInfo[0].split(',') + linesInfo[1].split(',')
+        linesInfo[0] = linesInfo[0][1:]
+        linesInfo[2] = linesInfo[2][1:] # by format: [old first line, old line count, new first line, new line count]
+        return (linesInfo, className)
+    return None
+
+def readDiffToDetectChangedMethods(diff):
+    for index, line in enumerate(diff.split('\n')):
+        chunkInfo = getFirstOfChunkInfo(line)
+        if chunkInfo:
+            print(chunkInfo)
+
 repo = '../repos/jdk7u-jdk'
 for commit in RepositoryMining(repo, only_modifications_with_file_types=['.java']).traverse_commits():
     for modification in commit.modifications:
@@ -11,5 +31,5 @@ for commit in RepositoryMining(repo, only_modifications_with_file_types=['.java'
                     needsAssess = True
                     break
             if needsAssess:
-                # TODO
+                readDiffToDetectChangedMethods(modification.diff)
                 pass
