fd8b9ed0785e95e7fad71ac5d2f2b38594d4d923
alinemati <alinemati@live.com>
2018-10-26 17:40:33 +0330

    #7 loop over modifications

--- mineRepoCommits.py
+++ mineRepoCommits.py
@@ -1,8 +1,9 @@
 from pydriller import RepositoryMining
+from pydriller.domain.commit import ModificationType
 
 repo = '../repos/jdk7u-jdk'
 for commit in RepositoryMining(repo, only_modifications_with_file_types=['.java']).traverse_commits():
     for mod in commit.modifications:
-        if len(mod.methods) > 0:
-            print(mod.methods[0].name, mod.methods[0].parameters)
+        if mod.change_type != ModificationType.ADD and mod.change_type != ModificationType.DELETE and mod.added > 0 and mod.removed > 0 and len(mod.methods) > 0:
+            pass
     break
