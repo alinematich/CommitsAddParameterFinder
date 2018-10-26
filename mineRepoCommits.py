46013b9dec127aef0a5d81b7ecc0074d432a5b56
alinemati <alinemati@live.com>
2018-10-26 22:39:27 +0330

    #8 detected changes

--- mineRepoCommits.py
+++ mineRepoCommits.py
@@ -3,7 +3,13 @@ from pydriller.domain.commit import ModificationType
 
 repo = '../repos/jdk7u-jdk'
 for commit in RepositoryMining(repo, only_modifications_with_file_types=['.java']).traverse_commits():
-    for mod in commit.modifications:
-        if mod.change_type != ModificationType.ADD and mod.change_type != ModificationType.DELETE and mod.added > 0 and mod.removed > 0 and len(mod.methods) > 0:
-            pass
-    break
+    for modification in commit.modifications:
+        if modification.change_type != ModificationType.ADD and modification.change_type != ModificationType.DELETE and modification.added > 0 and modification.removed > 0:
+            needsAssess = False
+            for method in modification.methods:
+                if len(method.parameters) > 0:
+                    needsAssess = True
+                    break
+            if needsAssess:
+                # TODO
+                pass
