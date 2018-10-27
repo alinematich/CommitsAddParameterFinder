d51e5bc59ef6e1364822b824fee8f61c2ceef45e
alinemati <alinemati@live.com>
2018-10-27 18:10:48 +0330

    #9 main attrs of javalang Method

--- mineRepoCommits.py
+++ mineRepoCommits.py
@@ -3,85 +3,59 @@ from pydriller.domain.commit import ModificationType
 import re
 import javalang
 
-class Method:
-    def __init__(self, method):
-        self.signature = re.sub(r'\s+', ' ' ,' '.join(method[:-1]))
-        self.name = method[4].strip()
-        args = method[5][1:-1].strip()
-        if args:
-            self.args = set(list(map(lambda arg: arg.replace(']', '] ').split()[-1], args.split(','))))
-        else:
-            self.args = set()
-
-    def hasLessArgsThan(self, other):
-        return len(self.args & other.args) < len(other.args)
-    
-    def __str__(self):
-        return self.signature
-        return self.name + '(' + ', '.join(self.args) + ')'
-
-    __repr__ = __str__
-
-    def __eq__(self, other):
-        return self.name == other.name
-
-def isFirstOfChunkInfo(line):
+def getFirstOfChunkInfo(line):
     infoParts = line.strip().split('@@')
-    if len(infoParts) == 3: # check if the format is correct
-        return True
-    return False
+    if len(infoParts) == 3 and line.startswith('@@'): # check if the format is correct
+        linesInfo = infoParts[1].split() 
+        linesInfo = linesInfo[0].split(',') + linesInfo[1].split(',')
+        linesInfo[0] = int(linesInfo[0][1:]) - 1
+        linesInfo[1] = int(linesInfo[1])
+        linesInfo[2] = int(linesInfo[2][1:]) - 1
+        linesInfo[3] = int(linesInfo[3]) # by format: [old first line, old line count, new first line, new line count]
+        return linesInfo
+    return None
 
-def getMethodDifferences(removedContent, addedContent):
-    regex = r'\s*(public|private|protected)?(\s+static)?(\s+final)?\s+(\w+)\s+(\w+)\s*(\([^\)]*\))\s*(throws [^\{]*)?\{'
-    oldMethods = sorted(list(map(lambda method: Method(method), re.findall(regex, removedContent))), key= lambda item: item.name)
-    newMethods = sorted(list(map(lambda method: Method(method), re.findall(regex, addedContent))), key= lambda item: item.name)
-    for oldMethod in oldMethods:
-        for newMethod in newMethods:
-            if oldMethod == newMethod and oldMethod.hasLessArgsThan(newMethod):
-                print(str(oldMethod) + ' --> ' + str(newMethod))
-                print()
-                break
 
-def readDiffToDetectChangedMethods(diff):
-    removedContent = ''
-    addedContent = ''
-    processing = False
-    modified = None
-    for index, line in enumerate(diff.split('\n')):
-        if isFirstOfChunkInfo(line): # it is chunk header
-            if processing:
-                processing = False
-                if modified: # it has both removed and added lines
-                    getMethodDifferences(removedContent, addedContent)
-                modified = None
-                removedContent = ''
-                addedContent = ''
+def getOldDocFromDiff(newDoc, diff):
+    oldDoc = []
+    newDoc = newDoc.split('\n')
+    diff = diff.split('\n')
+    chunkInfo = None
+    lineNum = 0
+    for index, line in enumerate(diff):
+        info = getFirstOfChunkInfo(line)
+        if info: # it is chunk header
+            chunkInfo = (index, info)
+            oldDoc += newDoc[lineNum : info[2]]
+            lineNum = info[2]
         else: # it is file content
             if line.startswith('-'):
-                processing = True
-                modified = False
-                removedContent += ' ' + line[1:]
+                oldDoc.append(line[1:])
             elif line.startswith('+'):
-                processing = True
-                if modified == False:
-                    modified = True
-                addedContent += ' ' + line[1:]
-            elif processing:
-                processing = False
-                if modified: # it has both removed and added lines
-                    getMethodDifferences(removedContent, addedContent)
-                modified = None
-                removedContent = ''
-                addedContent = ''
+                lineNum += 1
+            else:
+                try: 
+                    oldDoc.append(newDoc[lineNum])
+                except:
+                    break
+                lineNum += 1
+    return '\n'.join(oldDoc)
 
-def do(code):
+# constructor declaration
+
+def parse(code, filename):
     tree = javalang.parse.parse(code)
     for path, node in tree.filter(javalang.tree.MethodDeclaration):
         if node.parameters:
+            print(node)
+            print()
+            print(filename)
+            print()
             print(node.modifiers)
             print()
-            print(node.return_type)
-            print()
+            if node.return_type:
+                print(node.return_type.name)
+                print()
             print(node.name)
             print()
             print(node.parameters)
@@ -96,6 +70,7 @@ for commit in RepositoryMining(repo, only_modifications_with_file_types=['.java'
                 if method.parameters:
                     needsAssess = True
                     break
-            if needsAssess: # file may have "added paramerer methods" 
-                # readDiffToDetectChangedMethods(modification.diff)
-                do(modification.source_code)
+            if needsAssess: # file may have "added paramerer methods"
+                newDoc = modification.source_code
+                oldDoc = getOldDocFromDiff(newDoc, modification.diff)
+                parse(newDoc, modification.new_path)
