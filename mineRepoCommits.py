69c7ffea4eb3834a1117043aca7dde5ac418c59d
alinemati <alinemati@live.com>
2018-10-28 11:45:10 +0330

    #14 doing some optimizations and adding comments and saving in csv file

--- mineRepoCommits.py
+++ mineRepoCommits.py
@@ -1,58 +1,25 @@
 from pydriller import RepositoryMining
 from pydriller.domain.commit import ModificationType
 import javalang
-
-class Parameter:
-    def __init__(self, parameter):
-        self.name = parameter.name
-        self.type = parameter.type.name + '[]' * len(parameter.type.dimensions)
-
-    def __str__(self):
-        return self.type + ' ' + self.name
-
-    __repr__ = __str__
-
-    def __eq__(self, other):
-        return self.name == other.name
-
-class Method:
-    def __init__(self, method, path):
-        self.name = method.name
-        if path:
-            self.name = '::'.join(path) + '::' + method.name
-        self.parameters = list(map(Parameter, method.parameters))
-        return_type = 'void'
-        if method.return_type:
-            return_type = method.return_type.name + '[]' * len(method.return_type.dimensions)
-        modifiers = ' '.join(list(method.modifiers))
-        if modifiers:
-            modifiers += ' '
-        self.signature =  modifiers + return_type + ' ' + self.name + '(' + ', '.join(map(str, self.parameters)) + ')'
-    
-    def __str__(self):
-        return self.signature
-
-    __repr__ = __str__
-
-    def __eq__(self, other):
-        if not isinstance(other, Method):
-            return False
-        return self.name == other.name
-    
-    def __ne__(self, other):
-        return not self.__eq__(self, other)
+import csv
 
 def getFirstOfChunkInfo(line):
     infoParts = line.strip().split('@@')
-    if len(infoParts) == 3 and line.startswith('@@'): # check if the format is correct
+    if len(infoParts) == 3 and line.startswith('@@'): # validate hunk header format
         linesInfo = infoParts[1].split() 
-        linesInfo = linesInfo[0].split(',') + linesInfo[1].split(',')
+        oldInfo = linesInfo[0].split(',')
+        if len(oldInfo) == 1:
+            oldInfo.append(0)
+        newInfo = linesInfo[1].split(',')
+        if len(newInfo) == 1:
+            newInfo.append(0)
+        linesInfo =  oldInfo + newInfo
         linesInfo[0] = int(linesInfo[0][1:]) - 1
         linesInfo[1] = int(linesInfo[1])
         linesInfo[2] = int(linesInfo[2][1:]) - 1
-        linesInfo[3] = int(linesInfo[3]) # by format: [old first line, old line count, new first line, new line count]
-        return linesInfo
-    return None
+        linesInfo[3] = int(linesInfo[3])
+        return linesInfo # by format: [old first line, old line count, new first line, new line count]
+    return None # it is not hunk header
 
 def getOldDocFromDiff(newDoc, diff):
     oldDoc = []
@@ -60,6 +27,8 @@ def getOldDocFromDiff(newDoc, diff):
     diff = diff.split('\n')
     lineNum = 0
     for index, line in enumerate(diff):
+        if line.startswith('\\') or line == '' and index == len(diff) - 1: # sometimes "\ no newlines at the end of file" appears
+            continue
         chunkHeader = getFirstOfChunkInfo(line)
         if chunkHeader: # it is chunk header
             oldDoc += newDoc[lineNum : chunkHeader[2]]
@@ -75,95 +44,77 @@ def getOldDocFromDiff(newDoc, diff):
     oldDoc += newDoc[lineNum:]
     return '\n'.join(oldDoc)
 
-# constructor declaration
+def populateMethod(method, path): # modify method object and add variables that we need
+        if path:
+            method.name = '::'.join(path) + '::' + method.name
+        method.parameters = list(map(lambda parameter: parameter.type.name + '[]' * len(parameter.type.dimensions) + ' ' + parameter.name, method.parameters))
+        return_type = ''
+        if hasattr(method, 'return_type'): # not constructor
+            return_type = 'void'
+            if method.return_type: # not void
+                return_type = method.return_type.name + '[]' * len(method.return_type.dimensions)
+        modifiers = ' '.join(list(method.modifiers))
+        if modifiers:
+            modifiers += ' '
+        method.signature =  modifiers + return_type + ' ' + method.name + '(' + ', '.join(method.parameters) + ')'
 
 def parseMethods(code, diff):
-    tree = javalang.parse.parse(code)
+    tree = javalang.parse.parse(code) # creates AST from code string
     methods = []
     for path, node in tree.filter(javalang.tree.MethodDeclaration):
-        if node.name in diff:
-            methods.append(Method(node, map(lambda node: node.name, filter(lambda node: hasattr(node, 'name'), path))))
+        if node.name in diff: # if method doesn't appear in diff string then no parameter is added to it
+            populateMethod(node, map(lambda node: node.name, filter(lambda node: hasattr(node, 'name'), path)))
+            methods.append(node)
+    for path, node in tree.filter(javalang.tree.ConstructorDeclaration):
+        if node.name in diff: # if method doesn't appear in diff string then no parameter is added to it
+            populateMethod(node, map(lambda node: node.name, filter(lambda node: hasattr(node, 'name'), path)))
+            methods.append(node)
     methods.sort(key= lambda item: item.name)
-    res = []
+    res = [] # create an array of overloaded methods list
     tmpMethod = None
     for method in methods:
-        if method == tmpMethod:
+        if tmpMethod and method.name == tmpMethod.name:
             res[-1].append(method)
         else:
             res.append([method])
         tmpMethod = method
-    for ls in res:
-        ls.sort(key= lambda method: len(method.parameters))
-    return res
+    return res # format: [[method1, method2], [method3, method4]]
 
-def isMethodInSortedList(method, methods):
-    found = False
-    for iMethod in methods:
-        if method == iMethod:
-            found = True
-            if len(method.parameters) == len(iMethod.parameters):
-                intersection = len(set(method.parameters) & set(iMethod.parameters))
-                if len(method.parameters) == intersection:
-                    return True
-        elif found:
-            break
-    return False
-
-repo = '../repos/jdk7u-jdk' # repo path either local or remote
+repo = '../repos/java-design-patterns' # repo path either local or remote
+outfile = open('results.csv', mode='w')
+writer = csv.writer(outfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+writer.writerow(['Commit SHA', 'Java File', 'Old function signature', 'New function signature'])
+i = 1
 for commit in RepositoryMining(repo, only_modifications_with_file_types=['.java']).traverse_commits():
+    i += 1
+    if i % 100 == 0:
+        print(i)
     for modification in commit.modifications:
         if modification.filename.endswith('.java') and modification.change_type != ModificationType.ADD and modification.change_type != ModificationType.DELETE and modification.added > 0 and modification.removed > 0:
-            needsAssess = False
-            for method in modification.methods:
-                if method.parameters:
-                    needsAssess = True
-                    break
-            if needsAssess: # file may have "added paramerer methods"
-                newDoc = modification.source_code
-                oldDoc = getOldDocFromDiff(newDoc, modification.diff)
-                oldMethods = parseMethods(oldDoc, modification.diff)
-                newMethods = parseMethods(newDoc, modification.diff)
-                newMethodsNames = set(map(lambda methodlist: methodlist[0].name, newMethods))
-                oldMethodsNames = set(map(lambda methodlist: methodlist[0].name, oldMethods))
-                methodNamesIntersection = oldMethodsNames & newMethodsNames
-                while oldMethods:
-                    if oldMethods[0][0].name not in methodNamesIntersection:
-                        del oldMethods[0]
+            newDoc = modification.source_code
+            newMethods = parseMethods(newDoc, modification.diff)
+            newMethodsNames = set(map(lambda methodlist: methodlist[0].name, newMethods))
+            oldDoc = getOldDocFromDiff(newDoc, modification.diff)
+            oldMethods = parseMethods(oldDoc, modification.diff)
+            oldMethodsNames = set(map(lambda methodlist: methodlist[0].name, oldMethods))
+            methodNamesIntersection = oldMethodsNames & newMethodsNames # remove functions which are not in both files
+            for oldMethodlist in oldMethods:
+                if oldMethodlist[0].name not in methodNamesIntersection:
+                    continue
+                for newMethodlist in newMethods:
+                    if newMethodlist[0].name != oldMethodlist[0].name:
                         continue
-                    while newMethods:
-                        if newMethods[0][0].name not in methodNamesIntersection:
-                            del newMethods[0]
-                            continue
-                        oldSet = set(map(lambda method: frozenset(map(str, method.parameters)), oldMethods[0]))
-                        newSet = set(map(lambda method: frozenset(map(str, method.parameters)), newMethods[0]))
-                        intersection = oldSet & newSet
-                        oldSet -= intersection
-                        newSet -= intersection
-                        for newParameters in newSet:
-                            maxNum = 0
-                            counterOldParameters = None
-                            for oldParameters in oldSet:
-                                if newParameters >= oldParameters and len(oldParameters) > maxNum:
-                                    maxNum = len(oldParameters)
-                                    counterOldParameters = oldParameters
-                            if counterOldParameters:
-                                oldRes = None
-                                newRes = None
-                                for oldMethod in oldMethods[0]:
-                                    if not len(set(map(str, oldMethod.parameters)) ^ counterOldParameters):
-                                        oldRes = oldMethod
-                                        break
-                                for newMethod in newMethods[0]:
-                                    if not len(set(map(str, newMethod.parameters)) ^ newParameters):
-                                        newRes = newMethod
-                                        break
-                                print(commit.hash)
-                                print(modification.new_path)
-                                print(oldMethods[0][0].name)
-                                print(oldRes)
-                                print(newRes)
-                                print('------------################---------------')
-                        del newMethods[0]
-                        break
-                    del oldMethods[0]
+                    for newMethod in newMethodlist:
+                        maxNum = 0
+                        counterOldMethod = None
+                        newParamsSet = set(newMethod.parameters)
+                        for oldMethod in oldMethodlist:
+                            intersection = len(newParamsSet & set(oldMethod.parameters))
+                            if len(newParamsSet) > intersection and len(oldMethod.parameters) == intersection and len(oldMethod.parameters) > maxNum:
+                                maxNum = len(oldMethod.parameters)
+                                counterOldMethod = oldMethod
+                        if counterOldMethod: # added parameter detected
+                            writer.writerow([commit.hash, modification.new_path, counterOldMethod.signature, newMethod.signature])
+                    break
 
+outfile.close()
